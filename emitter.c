14699466ce129efe34ce4e80a039b4503d8925a1
唐鳳 <audreyt@audreyt.org>
2006-03-31 05:32:01 +0000

    r11863@not (orig r120):  ingy | 2006-02-11 19:51:19 +0900  * re-apply the utf8 scalar_fold kluge.

--- emitter.c
+++ emitter.c
@@ -778,7 +778,10 @@ syck_emitter_escape( SyckEmitter *e, char *src, long len )
     int i;
     for( i = 0; i < len; i++ )
     {
-        if( (src[i] < 0x20) || (0x7E < src[i]) )
+        /* XXX - scalar_fold overloaded to mean utf8 from Audrey Tang */
+        if( (e->style == scalar_fold)
+                ? ((src[i] < 0x20) && (0 < src[i]))
+                : ((src[i] < 0x20) || (0x7E < src[i])) )
         {
             syck_emitter_write( e, "\\", 1 );
             if( '\0' == src[i] )
