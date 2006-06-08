c28db2a9b1840c5da2f2f6177d28987a68f6cf4d
唐鳳 <audreyt@audreyt.org>
2006-06-08 13:47:48 +0000

    * perl_common.h: Avoid segfault on reused-and-freed av_fetch elements.

--- perl_common.h
+++ perl_common.h
@@ -62,7 +62,9 @@ void perl_syck_mark_emitter(SyckEmitter *e, SV *sv) {
             len = av_len((AV*)sv) + 1;
             for (i = 0; i < len; i++) {
                 SV** sav = av_fetch((AV*)sv, i, 0);
-                perl_syck_mark_emitter( e, *sav );
+                if (sav != NULL) {
+                    perl_syck_mark_emitter( e, *sav );
+                }
             }
             break;
         }
