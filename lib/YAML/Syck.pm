8d31eb0230fbbf19011f5dceaf2c1f6611cdbf81
唐鳳 <audreyt@audreyt.org>
2006-07-01 04:59:53 +0000

    * prerelease again

--- lib/YAML/Syck.pm
+++ lib/YAML/Syck.pm
@@ -4,7 +4,7 @@ use vars qw( @ISA @EXPORT $VERSION $ImplicitTyping $UseCode $LoadCode $DumpCode 
 use 5.00307;
 
 BEGIN {
-    $VERSION = '0.46_02';
+    $VERSION = '0.60';
     @EXPORT  = qw( Dump Load DumpFile LoadFile );
     @ISA     = qw( Exporter );
 
