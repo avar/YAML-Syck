d4a208bda823715639708bd3b12c39bb54e96e99
Todd Rinaldo <toddr@cpan.org>
2012-02-14 22:53:38 -0600

    Bump version to stable for release to public

--- lib/JSON/Syck.pm
+++ lib/JSON/Syck.pm
@@ -5,7 +5,7 @@ use Exporter;
 use YAML::Syck ();
 
 BEGIN {
-    $VERSION    = '0.43_01';
+    $VERSION    = '0.43';
     @EXPORT_OK  = qw( Load Dump LoadFile DumpFile );
     @ISA        = 'Exporter';
     *Load       = \&YAML::Syck::LoadJSON;
